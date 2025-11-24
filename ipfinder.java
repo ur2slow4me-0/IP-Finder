ca122f5d986a11fa7ac370238b03189ecb58cdaf
Ur2slow4me <paska.aditya23@gmail.com>
2025-11-24 14:48:26 +0800

    Add files via upload
    A java tool for find the IP address of your target URL

---
+++ ipfinder.java
@@ -0,0 +1,14 @@
+import java.net.InetAddress;
+
+public class ipfinder{
+    public static void main(String[] args){
+        try {
+            String url = "google.com"; //can be changed
+            InetAddress inet = InetAddress.getByName(url);
+            System.out.println("Host: " + url);
+            System.out.println("IP Address: " + inet.getHostAddress());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
